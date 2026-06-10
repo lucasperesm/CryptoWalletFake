2cb069b42651441ce2fd87bde8b3a7bc2b7cfd60
LUCAS PERES MOREIRA <lucasp.moreira+bradesco@bradesco.com.br>
2026-06-10 11:33:04 -0300

    fix: animation retirado

--- CryptoWallet/View/WalletView.swift
+++ CryptoWallet/View/WalletView.swift
@@ -12,94 +12,93 @@ struct WalletView: View {
     
     var body: some View {
         NavigationStack {
-        ZStack {
-            AppBackground {
-                VStack(spacing: 32) {
-                    
-                    Text("Wallet")
-                        .font(.title2)
-                        .bold()
-                        .foregroundColor(.white)
-                    
-                    VStack(spacing: 10) {
-                        Text("$\(viewModel.totalBalance, specifier: "%.0f")")
-                            .font(.system(size: 32, weight: .semibold))
+            ZStack {
+                AppBackground {
+                    VStack(spacing: 32) {
+                        
+                        Text("Wallet")
+                            .font(.title2)
+                            .bold()
                             .foregroundColor(.white)
                         
-                        //tod add lógica para converter entre green/red com base no profit
-                        Text("+$\(viewModel.profit, specifier: "%.2f")  (+\(viewModel.profitPercentage, specifier: "%.2f")%)")
-                            .foregroundColor(Color.green)
-                            .font(.system(size: 16, weight: .semibold))
-                        
-                        Text("Last 24 hours")
-                            .foregroundColor(.gray)
-                            .font(.system(size: 13))
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 22)
-                    .background(
-                        RoundedRectangle(cornerRadius: 18)
-                            .stroke(Color.white.opacity(0.15))
-                    )
-                    
-                    HStack(spacing: 14) {
-                        Button(action: {
-                            selectedView = .buy
-                        }) {
-                            ActionButton(title: "Buy", icon: "plus")
-                        }
-                        
-                        Button(action: {
-                            selectedView = .sell
-                        }) {
-                            ActionButton(title: "Sell", icon: "minus")
-                        }
-                    }
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 22)
-                            .fill(Color.white.opacity(0.08))
-                        
-                        GeometryReader { geo in
-                            let width = geo.size.width / 2
+                        VStack(spacing: 10) {
+                            Text("$\(viewModel.totalBalance, specifier: "%.0f")")
+                                .font(.system(size: 32, weight: .semibold))
+                                .foregroundColor(.white)
                             
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(
-                                    Color(red: 88/255, green: 52/255, blue: 190/255)
-                                )
-                                .padding(5)
-                                .frame(width: width)
-                                .offset(x: selectedTab == .all ? width : 0)
-                                .animation(.easeInOut(duration: 0.25), value: selectedTab)
+                            //tod add lógica para converter entre green/red com base no profit
+                            Text("+$\(viewModel.profit, specifier: "%.2f")  (+\(viewModel.profitPercentage, specifier: "%.2f")%)")
+                                .foregroundColor(Color.green)
+                                .font(.system(size: 16, weight: .semibold))
+                            
+                            Text("Last 24 hours")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 13))
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 22)
+                        .background(
+                            RoundedRectangle(cornerRadius: 18)
+                                .stroke(Color.white.opacity(0.15))
+                        )
+                        
+                        HStack(spacing: 14) {
+                            Button(action: {
+                                selectedView = .buy
+                            }) {
+                                ActionButton(title: "Buy", icon: "plus")
+                            }
+                            
+                            Button(action: {
+                                selectedView = .sell
+                            }) {
+                                ActionButton(title: "Sell", icon: "minus")
+                            }
                         }
                         
-                        HStack {
-                            Text("Your coins")
-                                .frame(maxWidth: .infinity)
-                                .padding(.vertical, 12)
-                                .contentShape(Rectangle())
-                                .onTapGesture {
-                                    selectedTab = .your
-                                }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 22)
+                                .fill(Color.white.opacity(0.08))
                             
-                            Text("All coins")
-                                .frame(maxWidth: .infinity)
-                                .padding(.vertical, 12)
-                                .contentShape(Rectangle())
-                                .onTapGesture {
-                                    selectedTab = .all
-                                }
+                            GeometryReader { geo in
+                                let width = geo.size.width / 2
+                                
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(
+                                        Color(red: 88/255, green: 52/255, blue: 190/255)
+                                    )
+                                    .padding(5)
+                                    .frame(width: width)
+                                    .offset(x: selectedTab == .all ? width : 0)
+                                    .animation(.easeInOut(duration: 0.25), value: selectedTab)
+                            }
+                            
+                            HStack {
+                                Text("Your coins")
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical, 12)
+                                    .contentShape(Rectangle())
+                                    .onTapGesture {
+                                        selectedTab = .your
+                                    }
+                                
+                                Text("All coins")
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical, 12)
+                                    .contentShape(Rectangle())
+                                    .onTapGesture {
+                                        selectedTab = .all
+                                    }
+                            }
+                            .foregroundColor(.white)
                         }
-                        .foregroundColor(.white)
+                        .frame(height: 44)
+                        
+                        coinsGrid
                     }
-                    .frame(height: 44)
-                    
-                    coinsGrid
+                    .padding(14)
                 }
-                .padding(14)
             }
-        }
-        .animation(.easeInOut(duration: 0.3), value: showTransactionScreen)
         }
     }
     
